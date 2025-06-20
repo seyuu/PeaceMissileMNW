f17c33ab4e8c03a64e0ab1957db9350f205e3b75
Serdar Yurtsever <serdaryurtsever@gmail.com>
2025-06-20 21:41:46 +0300

    webhook support 2

--- bot.py
+++ bot.py
@@ -1,15 +1,19 @@
 import os
 from telegram.ext import Application, CommandHandler
 
-async def start(update, context):
-    await update.message.reply_text('Hello!')
+TOKEN = os.environ.get("BOT_TOKEN") or "7620538088:AAGCKXgtDrzfg2jUnAY4WYp9rgwxNy6oOOE"
 
-app = Application.builder().token("7620538088:AAGCKXgtDrzfg2jUnAY4WYp9rgwxNy6oOOE").build()
+app = Application.builder().token(TOKEN).build()
+
+async def start(update, context):
+    await update.message.reply_text("Peace Missile Game Bot Online!")
+
 app.add_handler(CommandHandler("start", start))
 
 if __name__ == "__main__":
     app.run_webhook(
         listen="0.0.0.0",
         port=int(os.environ.get("PORT", 5000)),
-        webhook_url="https://peacemissile-bot-app-50391cca531c.herokuapp.com/7620538088:AAGCKXgtDrzfg2jUnAY4WYp9rgwxNy6oOOE"
+        webhook_url=f"https://peacemissile-bot-app-50391cca531c.herokuapp.com/{TOKEN}",
+        webhook_path=f"/{TOKEN}"
     )
