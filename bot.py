1e03fe09ce11e706569af3b77095dafa21724906
Serdar Yurtsever <serdaryurtsever@gmail.com>
2025-06-20 21:50:58 +0300

    test 1454

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import os
 from telegram.ext import Application, CommandHandler
 
 TOKEN = os.environ.get("BOT_TOKEN") or "7620538088:AAGCKXgtDrzfg2jUnAY4WYp9rgwxNy6oOOE"
+PORT = int(os.environ.get('PORT', 5000))
+WEBHOOK = f"https://peacemissile-bot-app-50391cca531c.herokuapp.com/{TOKEN}"
 
 app = Application.builder().token(TOKEN).build()
 
@@ -11,8 +13,9 @@ async def start(update, context):
 app.add_handler(CommandHandler("start", start))
 
 if __name__ == "__main__":
+    print("PEACEMISSILE BOT BAŞLIYOR!")
     app.run_webhook(
         listen="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000)),
-        webhook_url=f"https://peacemissile-bot-app-50391cca531c.herokuapp.com/{TOKEN}"        
+        port=PORT,
+        webhook_url=WEBHOOK
     )
